28997a03952baad20beaa596711e4ec0359e2e1f
pw911cn@163.com <pw911cn@163.com>
2019-03-01 17:59:41 +0800

    plugin组件，调通

--- vv_cloud_plugin/plugin_zuul/src/main/java/com/senyint/vv_cloud/plugin_zuul/Zuul_SpringApp.java
+++ vv_cloud_plugin/plugin_zuul/src/main/java/com/senyint/vv_cloud/plugin_zuul/Zuul_SpringApp.java
@@ -2,6 +2,8 @@ package com.senyint.vv_cloud.plugin_zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -9,26 +11,24 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  * 
  * 
  * @author peiwei
- * @desc Version: V4.0 Date: 2018年10月11日 下午5:41:28<br>
+ * @desc Version: V4.0 Date: 2018年10月19日 下午5:56:50<br>
  *       History:<br>
- *       Date Author Version Desc<br>
+ *       Date          Author          Version          Desc<br>
  *       ---------------------------------------------------------<br>
- *       2018年10月11日 peiwei 4.0.1 xxx<br>
+ *       2018年10月19日       peiwei          4.0.1             xxx<br>
  */
 @EnableZuulProxy
-//@EnableFeignClients
 @EnableDiscoveryClient
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-// @ComponentScan(basePackages = { "" })
 @SpringBootApplication
-public class Zuul_SpringApp {
+public class Zuul_SpringApp extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Zuul_SpringApp.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Zuul_SpringApp.class, args);
     }
 
-    //	@Bean
-    //	LoadBalancerInterceptor loadBalancerInterceptor(LoadBalancerClient loadBalance) {
-    //		return new LoadBalancerInterceptor(loadBalance);
-    //	}
 }
