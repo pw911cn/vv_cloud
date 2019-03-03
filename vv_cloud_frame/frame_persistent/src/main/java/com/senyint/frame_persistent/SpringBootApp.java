c8713a14ecf3672cfb1e4569141c2d9e64da8de7
pw911cn <pw911cn@163.com>
2019-03-03 22:20:04 +0800

    基本完整的结构

---
+++ vv_cloud_frame/frame_persistent/src/main/java/com/senyint/frame_persistent/SpringBootApp.java
@@ -0,0 +1,18 @@
+package com.senyint.frame_persistent;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author peiwei
+ * @desc Version: V4.0 Date: 2018年10月11日 下午5:40:15<br>
+ *       History:<br>
+ *       Date Author Version Desc<br>
+ *       ---------------------------------------------------------<br>
+ *       2018年10月11日 peiwei 4.0.1 xxx<br>
+ */
+public class SpringBootApp  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+}
